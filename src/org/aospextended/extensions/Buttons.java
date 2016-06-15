208a632002759d39568dcd0b9a05d832d190a6c2
GodfatherX64 <godfather.x64@gmail.com>
2016-06-15 12:51:05 +0200

    Enable single tap rebinding for hwkey back and home
    Initially, this policy was put in place to safeguard
    the UX by preventing a usecase where the device could
    be put in a state without back or home available. However,
    if a user puts the device in this state, it is easy enough
    to navigate to button settings to fix it
    
    Change-Id: I6b71f9e54fcb99356e07f236e9ed926ac5fc017e

--- src/org/aospextended/extensions/Buttons.java
+++ src/org/aospextended/extensions/Buttons.java
@@ -171,6 +171,12 @@ public class Buttons extends ActionFragment implements OnPreferenceChangeListene
     }
 
     @Override
+    protected boolean usesExtendedActionsList() {
+        return true;
+    }
+
+
+    @Override
     public void onResume() {
         super.onResume();
     }

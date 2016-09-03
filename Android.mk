bc4ebaa0ac5b8926621247066a9aeed4576b000b
Steve Kondik <shade@chemlab.org>
2016-09-03 05:15:27 -0700

    Extensions: Privacy Guard settings
     * Refactoring PG settings to live in Extensions
     * Includes general infrastructure and top-level settings
    
    Change-Id: If031b683bcef119852553c21b10078ddf6c46ad8
    Signed-off-by: Raleigh Matlock <raleighman2@gmail.com>
    Signed-off-by: ishubhamsingh <coolsks94@gmail.com>

---
+++ Android.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_PACKAGE_NAME := Extensions
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_MODULE_TAGS := optional
+LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+
+include $(BUILD_PACKAGE)

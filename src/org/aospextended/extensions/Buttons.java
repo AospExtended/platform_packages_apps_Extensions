c8a4de376ca9246fb1f20a8b77b84f552c501123
rascarlo <rascarlo@gmail.com>
2016-12-20 02:19:40 +0000

    Volume Rocker music controls [2/2] Change-Id: I44eb006f83ad154bcd3511bcbd2313dbda395b32

--- src/org/aospextended/extensions/Buttons.java
+++ src/org/aospextended/extensions/Buttons.java
@@ -59,9 +59,11 @@ public class Buttons extends SettingsPreferenceFragment implements OnPreferenceC
 
     private static final String SWAP_VOLUME_BUTTONS = "swap_volume_buttons";
     private static final String VOLUME_ROCKER_WAKE = "volume_rocker_wake";
+    public static final String VOLUME_ROCKER_MUSIC_CONTROLS = "volume_rocker_music_controls";
 
     private SwitchPreference mSwapVolumeButtons;
     private SwitchPreference mVolumeRockerWake;
+    private SwitchPreference mVolumeRockerMusicControl;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -85,6 +87,12 @@ public class Buttons extends SettingsPreferenceFragment implements OnPreferenceC
                 VOLUME_ROCKER_WAKE, 0);
         mVolumeRockerWake.setChecked(volumeRockerWake != 0);
 
+        mVolumeRockerMusicControl = (SwitchPreference) findPreference(VOLUME_ROCKER_MUSIC_CONTROLS);
+        mVolumeRockerMusicControl.setOnPreferenceChangeListener(this);
+        int volumeRockerMusicControl = Settings.System.getInt(getContentResolver(),
+                VOLUME_ROCKER_MUSIC_CONTROLS, 0);
+        mVolumeRockerMusicControl.setChecked(volumeRockerMusicControl != 0);
+
     }
 
     @Override
@@ -105,8 +113,13 @@ public class Buttons extends SettingsPreferenceFragment implements OnPreferenceC
                     value ? 1 : 0);
             return true;
         } else if (preference == mVolumeRockerWake) {
-            boolean value = (Boolean) newValue;
+            boolean value = (Boolean) objValue;
             Settings.System.putInt(getContentResolver(), VOLUME_ROCKER_WAKE,
+                    value ? 1 : 0);
+            return true;
+        } else if (preference == mVolumeRockerMusicControl) {
+            boolean value = (Boolean) objValue;
+            Settings.System.putInt(getContentResolver(), VOLUME_ROCKER_MUSIC_CONTROLS,
                     value ? 1 : 0);
             return true;
         }

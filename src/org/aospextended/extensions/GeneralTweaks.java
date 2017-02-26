0952f1b7ca2d4dd61330fd953f44784b32b240c3
Ezio Lacandia Bijelkic <brabus84@gmail.com>
2017-02-26 11:57:53 +0530

    Wired headset: make ringtone audio focus customizable [2/3] Change-Id: If244268df0803bd4b6c87afc57cbb3340f68f73f

--- src/org/aospextended/extensions/GeneralTweaks.java
+++ src/org/aospextended/extensions/GeneralTweaks.java
@@ -54,10 +54,12 @@ public class GeneralTweaks extends SettingsPreferenceFragment implements OnPrefe
     private static final String PREF_MEDIA_SCANNER_ON_BOOT = "media_scanner_on_boot";
     private static final String SCREENSHOT_TYPE = "screenshot_type";
     private static final String SCREENSHOT_DELAY = "screenshot_delay";
+    private static final String WIRED_RINGTONE_FOCUS_MODE = "wired_ringtone_focus_mode";
 
     private CustomSeekBarPreference mScreenshotDelay;
     private ListPreference mMsob;
     private ListPreference mScreenshotType;
+    private ListPreference mWiredHeadsetRingtoneFocus;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -69,13 +71,13 @@ public class GeneralTweaks extends SettingsPreferenceFragment implements OnPrefe
         final PreferenceScreen prefSet = getPreferenceScreen();
 
         mMsob = (ListPreference) findPreference(PREF_MEDIA_SCANNER_ON_BOOT);
-        mMsob.setValue(String.valueOf(Settings.System.getInt(getActivity().getContentResolver(),
+        mMsob.setValue(String.valueOf(Settings.System.getInt(resolver,
                 Settings.System.MEDIA_SCANNER_ON_BOOT, 0)));
         mMsob.setSummary(mMsob.getEntry());
         mMsob.setOnPreferenceChangeListener(this);
 
         mScreenshotType = (ListPreference) findPreference(SCREENSHOT_TYPE);
-        int mScreenshotTypeValue = Settings.System.getInt(getActivity().getContentResolver(),
+        int mScreenshotTypeValue = Settings.System.getInt(resolver,
                 Settings.System.SCREENSHOT_TYPE, 0);
         mScreenshotType.setValue(String.valueOf(mScreenshotTypeValue));
         mScreenshotType.setSummary(mScreenshotType.getEntry());
@@ -86,6 +88,13 @@ public class GeneralTweaks extends SettingsPreferenceFragment implements OnPrefe
                 Settings.System.SCREENSHOT_DELAY, 1000);
         mScreenshotDelay.setValue(screenshotDelay / 1);
         mScreenshotDelay.setOnPreferenceChangeListener(this);
+
+        mWiredHeadsetRingtoneFocus = (ListPreference) findPreference(WIRED_RINGTONE_FOCUS_MODE);
+        int mWiredHeadsetRingtoneFocusValue = Settings.Global.getInt(resolver,
+                Settings.Global.WIRED_RINGTONE_FOCUS_MODE, 1);
+        mWiredHeadsetRingtoneFocus.setValue(Integer.toString(mWiredHeadsetRingtoneFocusValue));
+        mWiredHeadsetRingtoneFocus.setSummary(mWiredHeadsetRingtoneFocus.getEntry());
+        mWiredHeadsetRingtoneFocus.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -100,8 +109,9 @@ public class GeneralTweaks extends SettingsPreferenceFragment implements OnPrefe
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        final ContentResolver resolver = getActivity().getContentResolver();
         if (preference == mMsob) {
-            Settings.System.putInt(getActivity().getContentResolver(),
+            Settings.System.putInt(resolver,
                     Settings.System.MEDIA_SCANNER_ON_BOOT,
                     Integer.valueOf(String.valueOf(newValue)));
             mMsob.setValue(String.valueOf(newValue));
@@ -111,14 +121,22 @@ public class GeneralTweaks extends SettingsPreferenceFragment implements OnPrefe
             int mScreenshotTypeValue = Integer.parseInt(((String) newValue).toString());
             mScreenshotType.setSummary(
                     mScreenshotType.getEntries()[mScreenshotTypeValue]);
-            Settings.System.putInt(getContentResolver(),
+            Settings.System.putInt(resolver,
                     Settings.System.SCREENSHOT_TYPE, mScreenshotTypeValue);
             mScreenshotType.setValue(String.valueOf(mScreenshotTypeValue));
             return true;
         } else if (preference == mScreenshotDelay) {
             int screenshotDelay = (Integer) newValue;
-            Settings.System.putInt(getActivity().getContentResolver(),
+            Settings.System.putInt(resolver,
                     Settings.System.SCREENSHOT_DELAY, screenshotDelay * 1);
+            return true;
+        } else if (preference == mWiredHeadsetRingtoneFocus) {
+            int mWiredHeadsetRingtoneFocusValue = Integer.valueOf((String) newValue);
+            int index = mWiredHeadsetRingtoneFocus.findIndexOfValue((String) newValue);
+            mWiredHeadsetRingtoneFocus.setSummary(
+                    mWiredHeadsetRingtoneFocus.getEntries()[index]);
+            Settings.Global.putInt(resolver, Settings.Global.WIRED_RINGTONE_FOCUS_MODE,
+                    mWiredHeadsetRingtoneFocusValue);
             return true;
         }
         return false;

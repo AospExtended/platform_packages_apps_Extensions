4197dafb8ce8a7ebffebb5c3b9c03ec1b4cce234
Shubham Singh <coolsks94@gmail.com>
2017-07-21 17:26:33 +0530

    Avoid NPE

--- src/org/aospextended/extensions/Buttons.java
+++ src/org/aospextended/extensions/Buttons.java
@@ -201,8 +201,12 @@ public class Buttons extends ActionFragment implements OnPreferenceChangeListene
                 mButtonBrightness.setOnPreferenceChangeListener(this);
          }
         } else {
+		if(mBacklightTimeout != null) {
             prefScreen.removePreference(mBacklightTimeout);
+		}
+		if(mButtonBrightness != null) {
             prefScreen.removePreference(mButtonBrightness);
+		}
         }
 
         mSwapVolumeButtons = (SwitchPreference) findPreference(SWAP_VOLUME_BUTTONS);

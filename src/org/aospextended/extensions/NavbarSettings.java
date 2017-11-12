6c07baedf8b231189e977d98029df288e71c0f40
Shubham Singh <coolsks94@gmail.com>
2017-11-12 20:56:28 +0530

    Add missing import
    Change-Id: If2374776665a85e47fd73a5495f31296994798e5

--- src/org/aospextended/extensions/NavbarSettings.java
+++ src/org/aospextended/extensions/NavbarSettings.java
@@ -25,6 +25,7 @@ import android.os.Bundle;
 import android.os.UserHandle;
 import android.os.RemoteException;
 import android.os.ServiceManager;
+import android.support.v14.preference.SwitchPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.PreferenceCategory;

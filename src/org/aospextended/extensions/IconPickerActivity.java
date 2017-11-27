840e7808ae0375261c130d4d0b22457df9946637
Shubham Singh <coolsks94@gmail.com>
2017-11-27 06:46:14 +0530

    Fix crash of IconPicker
    Change-Id: I0da026d9ed4b0856261e707b000e973d4700769a

--- src/org/aospextended/extensions/IconPickerActivity.java
+++ src/org/aospextended/extensions/IconPickerActivity.java
@@ -110,7 +110,7 @@ public class IconPickerActivity extends Activity implements DialogInterface.OnCa
                 String packageName = info.activityInfo.packageName;
                 Intent intent = new Intent();
                 intent.setClassName("com.android.settings",
-                        "com.dirtyunicorns.tweaks.IconPackGridActivity");
+                        "org.aospextended.extensions.IconPackGridActivity");
                 intent.putExtra("icon_package_name", packageName);
                 dialog.dismiss();
                 startActivityForResult(intent, ICON_PACK_ICON_RESULT);

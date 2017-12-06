58c677c3184dc5e38ff376332e641cdca6970f1c
bigrushdog <randall.rushing@gmail.com>
2017-12-05 23:35:42 -0800

    HwKey action binding preferences
    Same robust action library as used on DUI navigation suite. Supports
    single tap, double tap, and long press on most common hardware
    buttons.
    
    Change-Id: I6d1e788ff1e2c45369772d4e912877641282e22a

--- src/org/aospextended/extensions/Buttons.java
+++ src/org/aospextended/extensions/Buttons.java
@@ -38,6 +38,7 @@ import android.view.IWindowManager;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import com.android.internal.util.aospextended.AEXUtils;
+import org.aospextended.extensions.dui.ActionFragment;
 
 import java.util.Locale;
 import android.text.TextUtils;
@@ -46,15 +47,35 @@ import android.view.View;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.internal.utils.du.ActionConstants;
 import com.android.settings.Utils;
 
-public class Buttons extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+public class Buttons extends ActionFragment implements OnPreferenceChangeListener {
 
     private static final String TORCH_POWER_BUTTON_GESTURE = "torch_power_button_gesture";
     private static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
 
     private ListPreference mVolumeKeyCursorControl;
     private ListPreference mTorchPowerButton;
+
+    // category keys
+    private static final String CATEGORY_BACK = "back_key";
+    private static final String CATEGORY_HOME = "home_key";
+    private static final String CATEGORY_MENU = "menu_key";
+    private static final String CATEGORY_ASSIST = "assist_key";
+    private static final String CATEGORY_APPSWITCH = "app_switch_key";
+    private static final String CATEGORY_VOLUME = "volume_keys";
+    private static final String CATEGORY_POWER = "power_key";
+
+    // Masks for checking presence of hardware keys.
+    // Must match values in frameworks/base/core/res/res/values/config.xml
+    public static final int KEY_MASK_HOME = 0x01;
+    public static final int KEY_MASK_BACK = 0x02;
+    public static final int KEY_MASK_MENU = 0x04;
+    public static final int KEY_MASK_ASSIST = 0x08;
+    public static final int KEY_MASK_APP_SWITCH = 0x10;
+    public static final int KEY_MASK_CAMERA = 0x20;
+    public static final int KEY_MASK_VOLUME = 0x40;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -64,6 +85,58 @@ public class Buttons extends SettingsPreferenceFragment implements OnPreferenceC
 
         final ContentResolver resolver = getActivity().getContentResolver();
         final PreferenceScreen prefScreen = getPreferenceScreen();
+
+        // bits for hardware keys present on device
+        final int deviceKeys = getResources().getInteger(
+                com.android.internal.R.integer.config_deviceHardwareKeys);
+
+        // read bits for present hardware keys
+        final boolean hasHomeKey = (deviceKeys & KEY_MASK_HOME) != 0;
+        final boolean hasBackKey = (deviceKeys & KEY_MASK_BACK) != 0;
+        final boolean hasMenuKey = (deviceKeys & KEY_MASK_MENU) != 0;
+        final boolean hasAssistKey = (deviceKeys & KEY_MASK_ASSIST) != 0;
+        final boolean hasAppSwitchKey = (deviceKeys & KEY_MASK_APP_SWITCH) != 0;
+
+        // load categories and init/remove preferences based on device
+        // configuration
+        final PreferenceCategory backCategory = (PreferenceCategory) prefScreen
+                .findPreference(CATEGORY_BACK);
+        final PreferenceCategory homeCategory = (PreferenceCategory) prefScreen
+                .findPreference(CATEGORY_HOME);
+        final PreferenceCategory menuCategory = (PreferenceCategory) prefScreen
+                .findPreference(CATEGORY_MENU);
+        final PreferenceCategory assistCategory = (PreferenceCategory) prefScreen
+                .findPreference(CATEGORY_ASSIST);
+        final PreferenceCategory appSwitchCategory = (PreferenceCategory) prefScreen
+                .findPreference(CATEGORY_APPSWITCH);
+
+        // back key
+        if (!hasBackKey) {
+            prefScreen.removePreference(backCategory);
+        }
+
+        // home key
+        if (!hasHomeKey) {
+            prefScreen.removePreference(homeCategory);
+        }
+
+        // App switch key (recents)
+        if (!hasAppSwitchKey) {
+            prefScreen.removePreference(appSwitchCategory);
+        }
+
+        // menu key
+        if (!hasMenuKey) {
+            prefScreen.removePreference(menuCategory);
+        }
+
+        // search/assist key
+        if (!hasAssistKey) {
+            prefScreen.removePreference(assistCategory);
+        }
+
+        // let super know we can load ActionPreferences
+        onPreferenceScreenLoaded(ActionConstants.getDefaults(ActionConstants.HWKEYS));
 
         if (!AEXUtils.deviceHasFlashlight(getContext())) {
             Preference toRemove = prefScreen.findPreference(TORCH_POWER_BUTTON_GESTURE);

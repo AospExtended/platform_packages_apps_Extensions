bb8221bc530051430ce499882c6205b3479aba52
Simon Davie <nexx@nexxdesign.co.uk>
2018-02-17 09:19:43 -0500

    BatteryMeterDrawable: add hidden option (2/2)
    Change-Id: I72c6d614789dbe31ddc305a1fe95b8a9b0cbb9e7
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- src/org/aospextended/extensions/fragments/StatusbarBatteryStyle.java
+++ src/org/aospextended/extensions/fragments/StatusbarBatteryStyle.java
@@ -52,6 +52,7 @@ public class StatusbarBatteryStyle extends SettingsPreferenceFragment implements
     private static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
 
     private static final int STATUS_BAR_BATTERY_STYLE_TEXT = 2;
+    private static final int STATUS_BAR_BATTERY_STYLE_HIDDEN = 3;
 
     private ListPreference mStatusBarBatteryShowPercent;
     private ListPreference mStatusBarBattery;
@@ -117,7 +118,8 @@ public class StatusbarBatteryStyle extends SettingsPreferenceFragment implements
     }
 
     private void enableStatusBarBatteryDependents(int batteryIconStyle) {
-        if (batteryIconStyle == STATUS_BAR_BATTERY_STYLE_TEXT) {
+        if (batteryIconStyle == STATUS_BAR_BATTERY_STYLE_TEXT
+                || batteryIconStyle == STATUS_BAR_BATTERY_STYLE_HIDDEN) {
             mStatusBarBatteryShowPercent.setEnabled(false);
         } else {
             mStatusBarBatteryShowPercent.setEnabled(true);

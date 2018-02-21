0621ba7fcd25669a4b6d6317638b5de2634b1c97
ezio84 <brabus84@gmail.com>
2018-02-22 01:36:47 +0530

    Move sysui tuner percentage settings to custom battery settings [2/2]
    * Hide landscape battery mode for now
    
    Change-Id: I40f914bf7d6a68380944f7aa302e87c64949c82a
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- src/org/aospextended/extensions/StatusbarBatteryStyle.java
+++ src/org/aospextended/extensions/StatusbarBatteryStyle.java
@@ -51,6 +51,7 @@ public class StatusbarBatteryStyle extends SettingsPreferenceFragment implements
     private static final String BATTERY_STYLE = "battery_style";
 
     private ListPreference mBatteryIconStyle;
+    private SwitchPreference mBatteryPercentage;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -61,10 +62,20 @@ public class StatusbarBatteryStyle extends SettingsPreferenceFragment implements
         final ContentResolver resolver = getActivity().getContentResolver();
         final PreferenceScreen prefSet = getPreferenceScreen();
 
+
+        int batteryStyle = Settings.Secure.getInt(resolver,
+                Settings.Secure.STATUS_BAR_BATTERY_STYLE, 0);
         mBatteryIconStyle = (ListPreference) findPreference(BATTERY_STYLE);
-        mBatteryIconStyle.setValue(Integer.toString(Settings.Secure.getInt(resolver,
-                Settings.Secure.STATUS_BAR_BATTERY_STYLE, 0)));
+        mBatteryIconStyle.setValue(Integer.toString(batteryStyle));
         mBatteryIconStyle.setOnPreferenceChangeListener(this);
+
+        boolean show = Settings.System.getInt(resolver,
+                Settings.System.SHOW_BATTERY_PERCENT, 1) == 1;
+        mBatteryPercentage = (SwitchPreference) findPreference("show_battery_percent");
+        mBatteryPercentage.setChecked(show);
+        mBatteryPercentage.setOnPreferenceChangeListener(this);
+        boolean hideForcePercentage = batteryStyle == 6 || batteryStyle == 7; /*text or hidden style*/
+        mBatteryPercentage.setEnabled(!hideForcePercentage);
 
     }
 
@@ -84,6 +95,14 @@ public class StatusbarBatteryStyle extends SettingsPreferenceFragment implements
             int value = Integer.valueOf((String) objValue);
             Settings.Secure.putInt(getContentResolver(),
                     Settings.Secure.STATUS_BAR_BATTERY_STYLE, value);
+            boolean hideForcePercentage = value == 6 || value == 7;/*text or hidden style*/
+            mBatteryPercentage.setEnabled(!hideForcePercentage);
+            return true;
+        } else  if (preference == mBatteryPercentage) {
+            boolean value = (Boolean) objValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.SHOW_BATTERY_PERCENT, value ? 1 : 0);
+            mBatteryPercentage.setChecked(value);
             return true;
          }
         return false;

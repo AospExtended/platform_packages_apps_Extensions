84950766058f5fe60f3086ed193212c740c6bd94
Shubham Singh <coolsks94@gmail.com>
2018-03-23 14:21:39 +0530

    Extensions: Move aexstats from aexpapers
     just moves it from https://github.com/ishubhamsingh/AEXPapers/commit/6878adb8f4ddf244149169f4261fa494db4fa7d8 to extensions
    
    Change-Id: I8cb9cecf52a17955a00eed4e58fccea0a8c53a31
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

---
+++ src/org/aospextended/extensions/aexstats/models/ServerResponse.java
@@ -0,0 +1,20 @@
+package org.aospextended.extensions.aexstats.models;
+
+/**
+ * Created by ishubhamsingh on 25/9/17.
+ */
+
+public class ServerResponse {
+
+    private String result;
+    private String message;
+
+    public String getResult() {
+        return result;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}

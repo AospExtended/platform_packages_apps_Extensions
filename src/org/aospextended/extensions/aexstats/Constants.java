0106ef925dc9a3fa4f3b3c97a24f26a413f764f8
Shubham Singh <coolsks94@gmail.com>
2018-03-23 14:21:39 +0530

    Extensions: Move aexstats from aexpapers
     just moves it from https://github.com/ishubhamsingh/AEXPapers/commit/6878adb8f4ddf244149169f4261fa494db4fa7d8 to extensions
    
    Change-Id: I8cb9cecf52a17955a00eed4e58fccea0a8c53a31
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

---
+++ src/org/aospextended/extensions/aexstats/Constants.java
@@ -0,0 +1,22 @@
+package org.aospextended.extensions.aexstats;
+
+/**
+ * Created by ishubhamsingh on 25/9/17.
+ */
+
+public class Constants {
+    public static final String BASE_URL= "https://stats.aospextended.com/";
+    public static final String TAG= "AEXstats";
+    public static final String PUSH_OPERATION = "push";
+
+    public static final String SUCCESS = "success";
+    public static final String FAILURE = "failure";
+    public static final String IS_FIRST_LAUNCH = "isFirstLaunch";
+    public static final String LAST_BUILD_DATE = "lastBuild";
+
+    public static final String KEY_DEVICE = "ro.aex.device";
+    public static final String KEY_MODEL = "ro.product.model";
+    public static final String KEY_VERSION = "5.3";
+    public static final String KEY_BUILD_TYPE = "ro.extended.releasetype";
+    public static final String KEY_BUILD_DATE = "ro.build.date";
+}

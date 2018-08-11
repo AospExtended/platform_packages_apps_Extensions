15e856f1fe6fed2290868ecf8768273872eff6e5
Akhil Narang <akhilnarang.1999@gmail.com>
2018-08-11 04:31:37 +0000

    Extensions: Navigation: let people disable both gestures and navbar
    Change-Id: I15dcfd846af70df549849af95597599718ad5297
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>
    Signed-off-by: ShubhamB <shubhamprince111@gmail.com>

--- src/org/aospextended/extensions/NavbarSettings.java
+++ src/org/aospextended/extensions/NavbarSettings.java
@@ -164,7 +164,6 @@ public class NavbarSettings extends SettingsPreferenceFragment implements Prefer
 
     private void updateBarVisibleAndUpdatePrefs(boolean showing) {
         mNavbarVisibility.setChecked(showing);
-        mGestureNavigation.setChecked(!showing);
         mGestureNavigation.setEnabled(!showing);
         mNavInterface.setEnabled(mNavbarVisibility.isChecked());
         mNavGeneral.setEnabled(mNavbarVisibility.isChecked());
@@ -172,10 +171,9 @@ public class NavbarSettings extends SettingsPreferenceFragment implements Prefer
 
     private void updateGestures(boolean showing) {
         mGestureNavigation.setChecked(showing);
-        mNavbarVisibility.setChecked(!showing);
         mNavbarVisibility.setEnabled(!showing);
-        mNavInterface.setEnabled(mNavbarVisibility.isChecked());
-        mNavGeneral.setEnabled(mNavbarVisibility.isChecked());
+        mNavInterface.setEnabled(mNavbarVisibility.isEnabled());
+        mNavGeneral.setEnabled(mNavbarVisibility.isEnabled());
     }
 
     @Override
@@ -190,16 +188,12 @@ public class NavbarSettings extends SettingsPreferenceFragment implements Prefer
             boolean showing = ((Boolean)newValue);
             Settings.Secure.putInt(getContentResolver(), Settings.Secure.NAVIGATION_BAR_VISIBLE,
                     showing ? 1 : 0);
-            Settings.System.putInt(getContentResolver(), Settings.System.USE_BOTTOM_GESTURE_NAVIGATION,
-                    showing ? 0 : 1);
             updateBarVisibleAndUpdatePrefs(showing);
             return true;
         } else if (preference == mGestureNavigation) {
             boolean showing = ((Boolean)newValue);
             Settings.System.putInt(getContentResolver(), Settings.System.USE_BOTTOM_GESTURE_NAVIGATION,
                     showing ? 1 : 0);
-            Settings.Secure.putInt(getContentResolver(), Settings.Secure.NAVIGATION_BAR_VISIBLE,
-                    showing ? 0 : 1);
             updateGestures(showing);
             return true;
         } else if (preference == mBarHeightPort) {

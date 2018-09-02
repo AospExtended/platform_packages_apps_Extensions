353c3ac1bcf4de973bfd5e7488d0a58f0f522e8a
Altaf-Mahdi <altaf.mahdi@gmail.com>
2018-09-02 15:00:02 +0000

    Extensions:Option to disable brightness slider (2/2)
     * also add QuickSettings fragment
    
    Change-Id: I1d4181a0800f7eda5c9c942acee1c82237b781f7
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- src/org/aospextended/extensions/fragments/QuickSettings.java
+++ src/org/aospextended/extensions/fragments/QuickSettings.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 AospExtended ROM Project
+ * Copyright (C) 2017 AospExtended ROM Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.

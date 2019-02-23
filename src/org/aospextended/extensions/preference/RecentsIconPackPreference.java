3948182bb476727cda23fa338d2557270864efd9
Alex <du.alexcruz@gmail.com>
2019-02-23 02:02:31 -0600

    Recents icon pack: Align layout with Settings
    The previous layout was fine for Launcher 3 because it doesn't have margins.
    However, in Settings we do have margins and the layout stood out in a bad
    way. This commit fixes that by aligning it with other preferences that have
    an icon.
    
    Change-Id: I8ba10323c69ccd2166c73210803be4e2b36bf035

--- src/org/aospextended/extensions/preference/RecentsIconPackPreference.java
+++ src/org/aospextended/extensions/preference/RecentsIconPackPreference.java
@@ -20,6 +20,7 @@ import android.content.Context;
 import android.provider.Settings;
 import android.util.AttributeSet;
 import org.aospextended.extensions.preference.IconPackPreference;
+import com.android.settings.R;
 
 public class RecentsIconPackPreference extends IconPackPreference {
 
@@ -29,6 +30,7 @@ public class RecentsIconPackPreference extends IconPackPreference {
 
     public RecentsIconPackPreference(Context context, AttributeSet attrs) {
         super(context, attrs, 0);
+        setLayoutResource(R.layout.recents_iconpack_preference);
     }
 
     public RecentsIconPackPreference(Context context, AttributeSet attrs, int defStyleAttr) {

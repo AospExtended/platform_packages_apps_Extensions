88a6028b1ca43a0e653b95f05f979048a659ab49
Arghya Chanda <arghyac35@gmail.com>
2019-03-09 19:02:24 +0530

    VolumePanel: Remove media with media summary
    * Also remove the Key AUDIO_PANEL_VIEW_RINGER as we are not using it
    
    Change-Id: I2f6e19811a50a15083752cbbff829afa8ec04317
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- src/org/aospextended/extensions/fragments/VolumePanel.java
+++ src/org/aospextended/extensions/fragments/VolumePanel.java
@@ -108,10 +108,6 @@ public class VolumePanel extends SettingsPreferenceFragment
     public static void reset(Context mContext) {
         ContentResolver resolver = mContext.getContentResolver();
         Settings.System.putIntForUser(resolver,
-                Settings.System.AUDIO_PANEL_VIEW_MEDIA, 0, UserHandle.USER_CURRENT);
-        Settings.System.putIntForUser(resolver,
-                Settings.System.AUDIO_PANEL_VIEW_RINGER, 0, UserHandle.USER_CURRENT);
-        Settings.System.putIntForUser(resolver,
                 Settings.System.AUDIO_PANEL_VIEW_NOTIFICATION, 0, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
                 Settings.System.AUDIO_PANEL_VIEW_ALARM, 0, UserHandle.USER_CURRENT);

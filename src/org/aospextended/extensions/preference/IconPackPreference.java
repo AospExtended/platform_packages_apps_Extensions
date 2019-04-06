pixeldustproject-caf/android_vendor_support

support: Adapt IconPackPreference class from Launcher3
Pick and adapt the icon pack preference in order to work with
v7.support.

This class is generic and will let other classes further
implementations, for instance it's being used for
RecentsIconPackPreference so to set values inside another Preference
"holder".

Change-Id: I910876a43128f65101c3de91276cf8f964e223b6

src/com/abc/support/preferences/IconPackPreference.java
@@ -1,0 +1,203 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.abc.support.preferences;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.support.v7.preference.Preference;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.abc.support.R;
+
+public class IconPackPreference extends Preference {
+
+    private final PackageManager pm;
+
+    public IconPackPreference(Context context) {
+        this(context, null);
+    }
+
+    public IconPackPreference(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public IconPackPreference(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        setLayoutResource(R.layout.iconpack_preference);
+        pm = context.getPackageManager();
+        // Initialise with stored preferences
+        init();
+    }
+
+    // This method might be called outside for cases like onResume()
+    public void init() {
+        String currentPack = getPersistedString("");
+        if (currentPack.isEmpty()) {
+            setNone();
+        } else {
+            try {
+                ApplicationInfo info = pm.getApplicationInfo(currentPack, 0);
+                setIcon(info.loadIcon(pm));
+                setSummary(info.loadLabel(pm));
+            } catch (PackageManager.NameNotFoundException e) {
+                setNone();
+            }
+        }
+    }
+
+    private void setNone() {
+        setIcon(getContext().getResources().getDrawable(android.R.drawable.sym_def_app_icon));
+        setSummary(getContext().getResources().getString(R.string.default_iconpack_title));
+    }
+
+    @Override
+    protected void onClick() {
+        super.onClick();
+        showDialog();
+    }
+
+    protected void showDialog() {
+        final Map<String, IconPackInfo> packages = loadAvailableIconPacks();
+        final IconAdapter adapter = new IconAdapter(getContext(), packages, getPersistedString(""));
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setAdapter(adapter, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int position) {
+                String item = adapter.getItem(position);
+                if (!item.isEmpty()) {
+                    IconPackInfo packInfo = packages.get(item);
+                    if (packInfo != null) {
+                        setIcon(packInfo.icon);
+                        setSummary(packInfo.label);
+                        // Set selected icon pack
+                        persistString(packInfo.packageName);
+                    }
+                } else {
+                    setNone();
+                    // Set android default icon pack
+                    persistString("");
+                }
+            }
+        });
+        builder.show();
+    }
+
+    private Map<String, IconPackInfo> loadAvailableIconPacks() {
+        Map<String, IconPackInfo> iconPacks = new HashMap<>();
+        List<ResolveInfo> list;
+        list = pm.queryIntentActivities(new Intent("com.novalauncher.THEME"), 0);
+        list.addAll(pm.queryIntentActivities(new Intent("org.adw.launcher.icons.ACTION_PICK_ICON"), 0));
+        list.addAll(pm.queryIntentActivities(new Intent("com.dlto.atom.launcher.THEME"), 0));
+        list.addAll(pm.queryIntentActivities(new Intent("android.intent.action.MAIN").addCategory("com.anddoes.launcher.THEME"), 0));
+        for (ResolveInfo info : list) {
+            iconPacks.put(info.activityInfo.packageName, new IconPackInfo(info, pm));
+        }
+        return iconPacks;
+    }
+
+    private static class IconPackInfo {
+        String packageName;
+        CharSequence label;
+        Drawable icon;
+
+        IconPackInfo(ResolveInfo r, PackageManager packageManager) {
+            packageName = r.activityInfo.packageName;
+            icon = r.loadIcon(packageManager);
+            label = r.loadLabel(packageManager);
+        }
+
+        public IconPackInfo(String label, Drawable icon, String packageName) {
+            this.label = label;
+            this.icon = icon;
+            this.packageName = packageName;
+        }
+    }
+
+    private static class IconAdapter extends BaseAdapter {
+        ArrayList<IconPackInfo> mSupportedPackages;
+        LayoutInflater mLayoutInflater;
+        String mCurrentIconPack;
+
+        IconAdapter(Context context, Map<String, IconPackInfo> supportedPackages, String currentPack) {
+            mLayoutInflater = LayoutInflater.from(context);
+            mSupportedPackages = new ArrayList<>(supportedPackages.values());
+            Collections.sort(mSupportedPackages, new Comparator<IconPackInfo>() {
+                @Override
+                public int compare(IconPackInfo lhs, IconPackInfo rhs) {
+                    return lhs.label.toString().compareToIgnoreCase(rhs.label.toString());
+                }
+            });
+
+            Resources res = context.getResources();
+            String defaultLabel = context.getResources().getString(R.string.default_iconpack_title);
+            Drawable icon = res.getDrawable(android.R.drawable.sym_def_app_icon);
+            mSupportedPackages.add(0, new IconPackInfo(defaultLabel, icon, ""));
+            mCurrentIconPack = currentPack;
+        }
+
+        @Override
+        public int getCount() {
+            return mSupportedPackages.size();
+        }
+
+        @Override
+        public String getItem(int position) {
+            return mSupportedPackages.get(position).packageName;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = mLayoutInflater.inflate(R.layout.iconpack_dialog, null);
+            }
+            IconPackInfo info = mSupportedPackages.get(position);
+            TextView txtView = (TextView) convertView.findViewById(R.id.title);
+            txtView.setText(info.label);
+            ImageView imgView = (ImageView) convertView.findViewById(R.id.icon);
+            imgView.setImageDrawable(info.icon);
+            RadioButton radioButton = (RadioButton) convertView.findViewById(R.id.radio);
+            radioButton.setChecked(info.packageName.equals(mCurrentIconPack));
+            return convertView;
+        }
+    }
+}

AospExtended/platform_packages_apps_Extensions

Signed-off-by: Shubham Singh <coolsks94@gmail.com>

src/org/aospextended/extensions/preference/IconPackPreference.java
@@ -1,0 +1,203 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.aospextended.extensions.preference;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.support.v7.preference.Preference;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.android.settings.R;
+
+public class IconPackPreference extends Preference {
+
+    private final PackageManager pm;
+
+    public IconPackPreference(Context context) {
+        this(context, null);
+    }
+
+    public IconPackPreference(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public IconPackPreference(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        setLayoutResource(R.layout.iconpack_preference);
+        pm = context.getPackageManager();
+        // Initialise with stored preferences
+        init();
+    }
+
+    // This method might be called outside for cases like onResume()
+    public void init() {
+        String currentPack = getPersistedString("");
+        if (currentPack.isEmpty()) {
+            setNone();
+        } else {
+            try {
+                ApplicationInfo info = pm.getApplicationInfo(currentPack, 0);
+                setIcon(info.loadIcon(pm));
+                setSummary(info.loadLabel(pm));
+            } catch (PackageManager.NameNotFoundException e) {
+                setNone();
+            }
+        }
+    }
+
+    private void setNone() {
+        setIcon(getContext().getResources().getDrawable(android.R.drawable.sym_def_app_icon));
+        setSummary(getContext().getResources().getString(R.string.default_iconpack_title));
+    }
+
+    @Override
+    protected void onClick() {
+        super.onClick();
+        showDialog();
+    }
+
+    protected void showDialog() {
+        final Map<String, IconPackInfo> packages = loadAvailableIconPacks();
+        final IconAdapter adapter = new IconAdapter(getContext(), packages, getPersistedString(""));
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setAdapter(adapter, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int position) {
+                String item = adapter.getItem(position);
+                if (!item.isEmpty()) {
+                    IconPackInfo packInfo = packages.get(item);
+                    if (packInfo != null) {
+                        setIcon(packInfo.icon);
+                        setSummary(packInfo.label);
+                        // Set selected icon pack
+                        persistString(packInfo.packageName);
+                    }
+                } else {
+                    setNone();
+                    // Set android default icon pack
+                    persistString("");
+                }
+            }
+        });
+        builder.show();
+    }
+
+    private Map<String, IconPackInfo> loadAvailableIconPacks() {
+        Map<String, IconPackInfo> iconPacks = new HashMap<>();
+        List<ResolveInfo> list;
+        list = pm.queryIntentActivities(new Intent("com.novalauncher.THEME"), 0);
+        list.addAll(pm.queryIntentActivities(new Intent("org.adw.launcher.icons.ACTION_PICK_ICON"), 0));
+        list.addAll(pm.queryIntentActivities(new Intent("com.dlto.atom.launcher.THEME"), 0));
+        list.addAll(pm.queryIntentActivities(new Intent("android.intent.action.MAIN").addCategory("com.anddoes.launcher.THEME"), 0));
+        for (ResolveInfo info : list) {
+            iconPacks.put(info.activityInfo.packageName, new IconPackInfo(info, pm));
+        }
+        return iconPacks;
+    }
+
+    private static class IconPackInfo {
+        String packageName;
+        CharSequence label;
+        Drawable icon;
+
+        IconPackInfo(ResolveInfo r, PackageManager packageManager) {
+            packageName = r.activityInfo.packageName;
+            icon = r.loadIcon(packageManager);
+            label = r.loadLabel(packageManager);
+        }
+
+        public IconPackInfo(String label, Drawable icon, String packageName) {
+            this.label = label;
+            this.icon = icon;
+            this.packageName = packageName;
+        }
+    }
+
+    private static class IconAdapter extends BaseAdapter {
+        ArrayList<IconPackInfo> mSupportedPackages;
+        LayoutInflater mLayoutInflater;
+        String mCurrentIconPack;
+
+        IconAdapter(Context context, Map<String, IconPackInfo> supportedPackages, String currentPack) {
+            mLayoutInflater = LayoutInflater.from(context);
+            mSupportedPackages = new ArrayList<>(supportedPackages.values());
+            Collections.sort(mSupportedPackages, new Comparator<IconPackInfo>() {
+                @Override
+                public int compare(IconPackInfo lhs, IconPackInfo rhs) {
+                    return lhs.label.toString().compareToIgnoreCase(rhs.label.toString());
+                }
+            });
+
+            Resources res = context.getResources();
+            String defaultLabel = context.getResources().getString(R.string.default_iconpack_title);
+            Drawable icon = res.getDrawable(android.R.drawable.sym_def_app_icon);
+            mSupportedPackages.add(0, new IconPackInfo(defaultLabel, icon, ""));
+            mCurrentIconPack = currentPack;
+        }
+
+        @Override
+        public int getCount() {
+            return mSupportedPackages.size();
+        }
+
+        @Override
+        public String getItem(int position) {
+            return mSupportedPackages.get(position).packageName;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = mLayoutInflater.inflate(R.layout.iconpack_dialog, null);
+            }
+            IconPackInfo info = mSupportedPackages.get(position);
+            TextView txtView = (TextView) convertView.findViewById(R.id.title);
+            txtView.setText(info.label);
+            ImageView imgView = (ImageView) convertView.findViewById(R.id.icon);
+            imgView.setImageDrawable(info.icon);
+            RadioButton radioButton = (RadioButton) convertView.findViewById(R.id.radio);
+            radioButton.setChecked(info.packageName.equals(mCurrentIconPack));
+            return convertView;
+        }
+    }
+}
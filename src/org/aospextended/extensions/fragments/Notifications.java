207ce6c3a543269a534e613357aa6bd5194f58d2
AmolAmrit <amol.amrit03@outlook.com>
2019-08-08 13:55:53 +0200

    In-Call Vib: Add Footer
    * This was not a bug.
    
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>
    Change-Id: Ic0933ee27ec18fb458c62a203aa368ee21e9a90e

--- src/org/aospextended/extensions/fragments/Notifications.java
+++ src/org/aospextended/extensions/fragments/Notifications.java
@@ -82,6 +82,7 @@ public class Notifications extends SettingsPreferenceFragment implements OnPrefe
         }
 
         PreferenceCategory incallVibCategory = (PreferenceCategory) findPreference(INCALL_VIB_OPTIONS);
+         mFooterPreferenceMixin.createFooterPreference().setTitle(R.string.incall_vibrate_footer);
         if (!Utils.isVoiceCapable(getActivity())) {
                 prefSet.removePreference(incallVibCategory);
         }

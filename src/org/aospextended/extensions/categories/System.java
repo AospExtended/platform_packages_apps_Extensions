f8052874cff7baf0af09efecae8f7783703bdd5e
Shubham Singh <coolsks94@gmail.com>
2019-10-17 03:33:26 +0530

    Use bottom navbar for categories
    * Update colors for Q's dark theme
    * Add night resources
    
    Change-Id: Icac81a224f7d94899341e2b7064d6ce6aba341d0

--- src/org/aospextended/extensions/categories/System.java
+++ src/org/aospextended/extensions/categories/System.java
@@ -44,6 +44,7 @@ public class System extends SettingsPreferenceFragment implements
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.system);
+        setRetainInstance(true);
 
         ContentResolver resolver = getActivity().getContentResolver();
     }

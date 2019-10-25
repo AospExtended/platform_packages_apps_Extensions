4839d592e22fa7697af596d6ac11c11685cb278a
Aayush Gupta <aayushgupta219@gmail.com>
2019-10-25 08:28:48 +0000

    Extensions: Switch stats to new api
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: I7391e86000f7b7f81a4d16810bd6c8883c309143

--- src/org/aospextended/extensions/aexstats/Constants.java
+++ src/org/aospextended/extensions/aexstats/Constants.java
@@ -5,7 +5,7 @@ package org.aospextended.extensions.aexstats;
  */
 
 public class Constants {
-    public static final String BASE_URL= "https://apitmp.aospextended.com/";
+    public static final String BASE_URL= "https://api.aospextended.com/";
     public static final String TAG= "AEXstats";
     public static final String PUSH_OPERATION = "push";
 

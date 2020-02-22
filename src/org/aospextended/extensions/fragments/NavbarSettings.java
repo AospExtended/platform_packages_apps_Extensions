f513d28eed3acb9c8dbd39dc085209f49be2bf29
Alex <du.alexcruz@gmail.com>
2020-02-22 03:42:01 -0500

    Navbar: Hide custom application preference if not in use
    No reason to show these preferences not in use.
    
    Change-Id: I71133e4e96b177cd979fde0b2199f6ca9f62315a

--- src/org/aospextended/extensions/fragments/NavbarSettings.java
+++ src/org/aospextended/extensions/fragments/NavbarSettings.java
@@ -302,17 +302,17 @@ public class NavbarSettings extends SettingsPreferenceFragment implements OnPref
         navbarCheck();
         customAppCheck();
 
-        mBackLongPressCustomApp.setEnabled(mBackLongPress.getEntryValues()
+        mBackLongPressCustomApp.setVisible(mBackLongPress.getEntryValues()
                 [backlongpress].equals("16"));
-        mBackDoubleTapCustomApp.setEnabled(mBackDoubleTap.getEntryValues()
+        mBackDoubleTapCustomApp.setVisible(mBackDoubleTap.getEntryValues()
                 [backdoubletap].equals("16"));
-        mHomeLongPressCustomApp.setEnabled(mHomeLongPress.getEntryValues()
+        mHomeLongPressCustomApp.setVisible(mHomeLongPress.getEntryValues()
                 [homelongpress].equals("16"));
-        mHomeDoubleTapCustomApp.setEnabled(mHomeDoubleTap.getEntryValues()
+        mHomeDoubleTapCustomApp.setVisible(mHomeDoubleTap.getEntryValues()
                 [homedoubletap].equals("16"));
-        mAppSwitchLongPressCustomApp.setEnabled(mAppSwitchLongPress.getEntryValues()
+        mAppSwitchLongPressCustomApp.setVisible(mAppSwitchLongPress.getEntryValues()
                 [appswitchlongpress].equals("16"));
-        mAppSwitchDoubleTapCustomApp.setEnabled(mAppSwitchDoubleTap.getEntryValues()
+        mAppSwitchDoubleTapCustomApp.setVisible(mAppSwitchDoubleTap.getEntryValues()
                 [appswitchdoubletap].equals("16"));
 		
         mHandler = new Handler();
@@ -482,7 +482,7 @@ public class NavbarSettings extends SettingsPreferenceFragment implements OnPref
             mBackLongPress.setSummary(
                     mBackLongPress.getEntries()[index]);
             customAppCheck();
-            mBackLongPressCustomApp.setEnabled(mBackLongPress.getEntryValues()
+            mBackLongPressCustomApp.setVisible(mBackLongPress.getEntryValues()
                     [index].equals("16"));
             return true;
         } else if (preference == mBackDoubleTap) {
@@ -493,7 +493,7 @@ public class NavbarSettings extends SettingsPreferenceFragment implements OnPref
             int index = mBackDoubleTap.findIndexOfValue((String) objValue);
             mBackDoubleTap.setSummary(
                     mBackDoubleTap.getEntries()[index]);
-            mBackDoubleTapCustomApp.setEnabled(mBackDoubleTap.getEntryValues()
+            mBackDoubleTapCustomApp.setVisible(mBackDoubleTap.getEntryValues()
                     [index].equals("16"));
             return true;
         } else if (preference == mHomeLongPress) {
@@ -504,7 +504,7 @@ public class NavbarSettings extends SettingsPreferenceFragment implements OnPref
             int index = mHomeLongPress.findIndexOfValue((String) objValue);
             mHomeLongPress.setSummary(
                     mHomeLongPress.getEntries()[index]);
-            mHomeLongPressCustomApp.setEnabled(mHomeLongPress.getEntryValues()
+            mHomeLongPressCustomApp.setVisible(mHomeLongPress.getEntryValues()
                     [index].equals("16"));
             return true;
         } else if (preference == mHomeDoubleTap) {
@@ -515,7 +515,7 @@ public class NavbarSettings extends SettingsPreferenceFragment implements OnPref
             int index = mHomeDoubleTap.findIndexOfValue((String) objValue);
             mHomeDoubleTap.setSummary(
                     mHomeDoubleTap.getEntries()[index]);
-            mHomeDoubleTapCustomApp.setEnabled(mHomeDoubleTap.getEntryValues()
+            mHomeDoubleTapCustomApp.setVisible(mHomeDoubleTap.getEntryValues()
                     [index].equals("16"));
             return true;
         } else if (preference == mAppSwitchLongPress) {
@@ -526,7 +526,7 @@ public class NavbarSettings extends SettingsPreferenceFragment implements OnPref
             int index = mAppSwitchLongPress.findIndexOfValue((String) objValue);
             mAppSwitchLongPress.setSummary(
                     mAppSwitchLongPress.getEntries()[index]);
-            mAppSwitchLongPressCustomApp.setEnabled(mAppSwitchLongPress.getEntryValues()
+            mAppSwitchLongPressCustomApp.setVisible(mAppSwitchLongPress.getEntryValues()
                     [index].equals("16"));
             return true;
         } else if (preference == mAppSwitchDoubleTap) {
@@ -537,7 +537,7 @@ public class NavbarSettings extends SettingsPreferenceFragment implements OnPref
             int index = mAppSwitchDoubleTap.findIndexOfValue((String) objValue);
             mAppSwitchDoubleTap.setSummary(
                     mAppSwitchDoubleTap.getEntries()[index]);
-            mAppSwitchDoubleTapCustomApp.setEnabled(mAppSwitchDoubleTap.getEntryValues()
+            mAppSwitchDoubleTapCustomApp.setVisible(mAppSwitchDoubleTap.getEntryValues()
                     [index].equals("16"));
             return true;
         } else if (preference == mMenuLongPress) {

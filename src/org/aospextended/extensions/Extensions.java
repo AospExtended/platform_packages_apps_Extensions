6fe744b99556fa91c34016c90b126e13f78eab39
Gaurav Singh <allstargaurav@gmail.com>
2020-03-23 11:58:48 +0530

    Extensions: Cleanup | Fix alignments | Order sub categories alphabetically | Minor UI/UX changes
    Change-Id: I94181383778296e6046795ca2f1e3c6042e82f53

--- src/org/aospextended/extensions/Extensions.java
+++ src/org/aospextended/extensions/Extensions.java
@@ -19,52 +19,48 @@ package org.aospextended.extensions;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
-import androidx.fragment.app.DialogFragment;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-import android.content.Context;
 import android.content.ContentResolver;
-import android.content.SharedPreferences;
-import android.util.Log;
+import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.Uri;
-import android.text.Html;
+import android.content.res.Resources;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.SystemProperties;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.os.Bundle;
-import android.os.Build;
-import android.os.SystemProperties;
-import android.provider.Settings;
-import androidx.core.content.ContextCompat;
-import androidx.preference.CheckBoxPreference;
-import androidx.preference.Preference;
-import androidx.preference.PreferenceCategory;
-import androidx.preference.PreferenceManager;
-import androidx.preference.Preference.OnPreferenceChangeListener;
-import androidx.preference.PreferenceScreen;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Toast;
 
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+
+import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.dashboard.SummaryLoader;
-
-import java.util.ArrayList;
-import java.util.List;
-import android.text.TextUtils;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomnavigation.LabelVisibilityMode;
 
 import org.aospextended.extensions.aexstats.Constants;
 import org.aospextended.extensions.aexstats.RequestInterface;
 import org.aospextended.extensions.aexstats.models.ServerRequest;
 import org.aospextended.extensions.aexstats.models.ServerResponse;
 import org.aospextended.extensions.aexstats.models.StatsData;
+import org.aospextended.extensions.categories.Lockscreen;
+import org.aospextended.extensions.categories.NavigationAndRecents;
+import org.aospextended.extensions.categories.NotificationsPanel;
+import org.aospextended.extensions.categories.StatusBar;
+import org.aospextended.extensions.categories.System;
 
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
@@ -72,14 +68,6 @@ import io.reactivex.schedulers.Schedulers;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.bottomnavigation.LabelVisibilityMode;
-
-import org.aospextended.extensions.categories.StatusBar;
-import org.aospextended.extensions.categories.NotificationsPanel;
-import org.aospextended.extensions.categories.NavigationAndRecents;
-import org.aospextended.extensions.categories.Lockscreen;
-import org.aospextended.extensions.categories.System;
 
 public class Extensions extends SettingsPreferenceFragment implements   
        Preference.OnPreferenceChangeListener {

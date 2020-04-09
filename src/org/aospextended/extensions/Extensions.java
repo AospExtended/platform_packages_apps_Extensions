0db25630d657caad20f27bb12e4706155c7a2b64
dmd79 <davidevinavil@gmail.com>
2020-04-09 16:59:35 +0200

    Revert "aexstats: Use custom fingerprint to determine new build"
    This reverts commit 93d93a73005c5f87a8bf311b8adc36cb683b326c.
    
    Change-Id: I5cac6c93cae7066261f90039cfbc2cb0ef58171c

--- src/org/aospextended/extensions/Extensions.java
+++ src/org/aospextended/extensions/Extensions.java
@@ -36,7 +36,6 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.os.Bundle;
-import android.os.Build;
 import android.os.SystemProperties;
 import android.provider.Settings;
 import androidx.core.content.ContextCompat;
@@ -147,7 +146,7 @@ public class Extensions extends SettingsPreferenceFragment implements
         ContentResolver resolver = getActivity().getContentResolver();
         mCompositeDisposable = new CompositeDisposable();
         pref = getActivity().getSharedPreferences("aexStatsPrefs", Context.MODE_PRIVATE);
-        if (!pref.getString(Constants.LAST_CUSTOM_FINGERPRINT, "null").equals(Build.CUSTOM_FINGERPRINT)
+        if (!pref.getString(Constants.LAST_BUILD_DATE, "null").equals(SystemProperties.get(Constants.KEY_BUILD_DATE))
                 || pref.getBoolean(Constants.IS_FIRST_LAUNCH, true)) {
             pushStats();
         }
@@ -196,7 +195,7 @@ public class Extensions extends SettingsPreferenceFragment implements
 
             SharedPreferences.Editor editor = pref.edit();
             editor.putBoolean(Constants.IS_FIRST_LAUNCH, false);
-            editor.putString(Constants.LAST_CUSTOM_FINGERPRINT, Build.CUSTOM_FINGERPRINT);
+            editor.putString(Constants.LAST_BUILD_DATE, SystemProperties.get(Constants.KEY_BUILD_DATE));
             editor.apply();
             Log.d(Constants.TAG, "push successful");
 

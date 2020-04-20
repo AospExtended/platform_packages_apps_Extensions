eca5f19f7651c927db97f7b654021272b001b0b7
ShubhamB <shubhamprince111@gmail.com>
2020-04-20 12:08:24 +0000

    Extensions: We missed this?
    Signed-off-by: ShubhamB <shubhamprince111@gmail.com>
    Change-Id: I568f8698ba15d39670b39348931317879521b45a

--- src/org/aospextended/extensions/categories/Lockscreen.java
+++ src/org/aospextended/extensions/categories/Lockscreen.java
@@ -21,12 +21,12 @@ import android.content.ContentResolver;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.UserHandle;
-import android.preference.ListPreference;
-import android.preference.SwitchPreference;
-import android.preference.Preference;
-import android.preference.PreferenceCategory;
-import android.preference.PreferenceScreen;
-import android.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.ListPreference;
+import androidx.preference.SwitchPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
 import android.provider.Settings;
 
 import com.android.settings.R;

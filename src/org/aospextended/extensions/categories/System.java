2c56e87b314dc39a9a5cf88d7fbfc65eabc1b304
ShubhamB <shubhamprince111@gmail.com>
2020-05-08 22:00:34 +0530

    Extensions: Battery: Change the logic of hiding Battery preference
    * Hide preference only if both overlays are false
    
    Signed-off-by: ShubhamB <shubhamprince111@gmail.com>
    Change-Id: I4d4c016a7d46ab0db546fb928e4fe14a73582c58
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- src/org/aospextended/extensions/categories/System.java
+++ src/org/aospextended/extensions/categories/System.java
@@ -47,15 +47,16 @@ public class System extends SettingsPreferenceFragment implements
 
         ContentResolver resolver = getActivity().getContentResolver();
 
-        Preference mBattery = (Preference) findPreference(PREF_BATTERY);
-        if (!hasBatteryLights(getContext()))
+        Preference mBattery = findPreference(PREF_BATTERY);
+        if (mBattery != null
+                && !getResources().getBoolean(
+                        com.android.internal.R.bool.config_enableSmartPixels)
+                && !getResources().getBoolean(
+                        com.android.internal.R.bool.config_intrusiveBatteryLed)) {
             getPreferenceScreen().removePreference(mBattery);
+        }
     }
 
-    private static boolean hasBatteryLights(Context context) {
-        return context.getResources().getBoolean(
-                com.android.internal.R.bool.config_intrusiveBatteryLed);
-    }
 
     @Override
     public int getMetricsCategory() {

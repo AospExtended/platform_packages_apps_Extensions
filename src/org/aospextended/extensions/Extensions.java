aeb94f1fdca31a72ed1efa101b878e0f2f6c54ba
ShubhamB <shubhamprince111@gmail.com>
2020-08-28 15:25:21 +0000

    Extensions: Remove custom fingeprrint
    Signed-off-by: ShubhamB <shubhamprince111@gmail.com>
    Change-Id: Ie1b468b3f4d287323e1a70d677798c3be1f564d0

--- src/org/aospextended/extensions/Extensions.java
+++ src/org/aospextended/extensions/Extensions.java
@@ -147,8 +147,7 @@ public class Extensions extends SettingsPreferenceFragment implements
         ContentResolver resolver = getActivity().getContentResolver();
         mCompositeDisposable = new CompositeDisposable();
         pref = getActivity().getSharedPreferences("aexStatsPrefs", Context.MODE_PRIVATE);
-        if (!pref.getString(Constants.LAST_CUSTOM_FINGERPRINT, "null").equals(Build.CUSTOM_FINGERPRINT)
-                || pref.getBoolean(Constants.IS_FIRST_LAUNCH, true)) {
+        if (pref.getBoolean(Constants.IS_FIRST_LAUNCH, true)) {
             pushStats();
         }
     }
@@ -196,7 +195,6 @@ public class Extensions extends SettingsPreferenceFragment implements
 
             SharedPreferences.Editor editor = pref.edit();
             editor.putBoolean(Constants.IS_FIRST_LAUNCH, false);
-            editor.putString(Constants.LAST_CUSTOM_FINGERPRINT, Build.CUSTOM_FINGERPRINT);
             editor.apply();
             Log.d(Constants.TAG, "push successful");
 

35ee80435d7c47ab014bd94e70cc271629968400
SpiritCroc <dev@spiritcroc.de>
2020-10-22 05:26:36 +0000

    Theme settings dashboard icons [3/3]
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Change-Id: I774b29ba4fd4bd55736b2ea06dc4e3daf78d5de7

--- src/org/aospextended/extensions/preference/CardviewPreference.java
+++ src/org/aospextended/extensions/preference/CardviewPreference.java
@@ -18,13 +18,17 @@ package org.aospextended.extensions.preference;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.View;
-
+import android.graphics.drawable.LayerDrawable;
+import android.graphics.drawable.ShapeDrawable;
 import androidx.core.content.res.TypedArrayUtils;
+import androidx.core.graphics.ColorUtils;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceViewHolder;
-
+import android.provider.Settings;
+import android.widget.ImageView;
 import com.android.settings.R;
 
 public class CardviewPreference extends Preference {
@@ -33,6 +37,12 @@ public class CardviewPreference extends Preference {
 
     private boolean mAllowDividerAbove;
     private boolean mAllowDividerBelow;
+
+    private ImageView arrow_icon;
+
+    private int mIconStyle;
+    private int mNormalColor;
+    private int mAccentColor;
 
     public CardviewPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -46,6 +56,7 @@ public class CardviewPreference extends Preference {
         a.recycle();
 
         setLayoutResource(R.layout.preference_cardview);
+        updateTheme();
     }
 
     public CardviewPreference(Context context, View view) {
@@ -62,5 +73,71 @@ public class CardviewPreference extends Preference {
         holder.itemView.setClickable(selectable);
         holder.setDividerAllowedAbove(mAllowDividerAbove);
         holder.setDividerAllowedBelow(mAllowDividerBelow);
+
+        arrow_icon = (ImageView) holder.findViewById(R.id.arrow_icon);
+
+
+        mIconStyle = Settings.System.getInt(getContext().getContentResolver(),
+                Settings.System.THEMING_SETTINGS_DASHBOARD_ICONS, 0);
+
+        if (arrow_icon != null) {
+            Drawable arrow = arrow_icon.getDrawable();
+            arrow.setTint(mIconStyle == 2 ? mNormalColor : mAccentColor);
+        }
+        updateTheme();
+    }
+
+    public void updateTheme() {
+        int[] attrs = new int[] {
+            android.R.attr.colorControlNormal,
+            android.R.attr.colorAccent,
+        };
+        TypedArray ta = getContext().getTheme().obtainStyledAttributes(attrs);
+        mNormalColor = ta.getColor(0, 0xff808080);
+        mAccentColor = ta.getColor(1, 0xff808080);
+        ta.recycle();
+
+        mIconStyle = Settings.System.getInt(getContext().getContentResolver(),
+                Settings.System.THEMING_SETTINGS_DASHBOARD_ICONS, 0);
+
+        if (arrow_icon != null) {
+            Drawable arrow = arrow_icon.getDrawable();
+            arrow.setTint(mIconStyle == 2 ? mNormalColor : mAccentColor);
+        }
+
+        Drawable icon = getIcon();
+        if (icon != null) {
+            if (icon instanceof LayerDrawable) {
+                LayerDrawable lIcon = (LayerDrawable) icon;
+                if (lIcon.getNumberOfLayers() == 2) {
+                    Drawable fg = lIcon.getDrawable(1);
+                    Drawable bg = lIcon.getDrawable(0);
+                    // Clear tints from previous calls
+                    bg.setTintList(null);
+                    fg.setTintList(null);
+                    int bgc = ((ShapeDrawable) bg).getPaint().getColor();
+                    switch (mIconStyle) {
+                        case 1:
+                            bg.setTint(mAccentColor);
+                            break;
+                        case 2:
+                            fg.setTint(mNormalColor);
+                            bg.setTint(0);
+                            break;
+                        case 3:
+                            fg.setTint(mAccentColor);
+                            bg.setTint(0);
+                            break;
+                        case 4:
+                            fg.setTint(mAccentColor);
+                            bg.setTint(ColorUtils.setAlphaComponent(mAccentColor, 51));
+                            break;
+                        case 5:
+                            fg.setTint(bgc);
+                            bg.setTint(ColorUtils.setAlphaComponent(bgc, 51));
+                    }
+                }
+            }
+        }
     }
 }

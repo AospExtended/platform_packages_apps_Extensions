c9b7040e598b45d78c4b5957f6ed3f20f616d6c4
SagarMakhar <sagarmakhar@gmail.com>
2020-11-07 18:21:26 +0000

    Add lockscreen clock options
    - while we don't ship themepicker, let's add these options here
    
    Change-Id: I4ada7d90bd439c79836061b0eb21143fc6e25d4c
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- src/org/aospextended/extensions/fragments/Customisation.java
+++ src/org/aospextended/extensions/fragments/Customisation.java
@@ -17,6 +17,7 @@
 package org.aospextended.extensions.fragments;
 
 import static android.os.UserHandle.USER_SYSTEM;
+import static android.os.UserHandle.USER_CURRENT;
 
 import android.app.ActivityManagerNative;
 import android.app.UiModeManager;
@@ -66,6 +67,9 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class Customisation extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
 
     private static final String TAG = "Customisation";
@@ -74,9 +78,12 @@ public class Customisation extends SettingsPreferenceFragment implements OnPrefe
     private static final String SYSTEM_ICON_STYLE = "android.theme.customization.icon_pack.android";
     private static final String SYSTEM_FONT_STYLE = "android.theme.customization.font";
 
+    private static final String CUSTOM_CLOCK_FACE = Settings.Secure.LOCK_SCREEN_CUSTOM_CLOCK_FACE;
+    private static final String DEFAULT_CLOCK = "com.android.keyguard.clock.DefaultClockController";
 
     private ListPreference mSystemThemeStyle;
     private ListPreference mIconPreference;
+    private ListPreference mLockClockStyles;
 
     private FontListPreference mFontPreference;
 
@@ -106,6 +113,12 @@ public class Customisation extends SettingsPreferenceFragment implements OnPrefe
         mFontPreference = (FontListPreference) screen.findPreference(SYSTEM_FONT_STYLE);
         mFontPreference.setOnPreferenceChangeListener(this);
         updateState((ListPreference) mFontPreference);
+
+        mLockClockStyles = (ListPreference) findPreference(CUSTOM_CLOCK_FACE);
+        String mLockClockStylesValue = getLockScreenCustomClockFace();
+        mLockClockStyles.setValue(mLockClockStylesValue);
+        mLockClockStyles.setSummary(mLockClockStyles.getEntry());
+        mLockClockStyles.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -136,6 +149,12 @@ public class Customisation extends SettingsPreferenceFragment implements OnPrefe
             mThemeUtils.setOverlayEnabled(SYSTEM_FONT_STYLE, (String) newValue);
             return true;
         }
+        if (preference == mLockClockStyles) {
+            setLockScreenCustomClockFace((String) newValue);
+            int index = mLockClockStyles.findIndexOfValue((String) newValue);
+            mLockClockStyles.setSummary(mLockClockStyles.getEntries()[index]);
+            return true;
+        }
         return false;
     }
 
@@ -162,5 +181,29 @@ public class Customisation extends SettingsPreferenceFragment implements OnPrefe
         preference.setEntryValues(pkgs.toArray(new String[pkgs.size()]));
         preference.setValue("Default".equals(currentPackageName) ? pkgs.get(0) : currentPackageName);
         preference.setSummary("Default".equals(currentPackageName) ? "Default" : labels.get(pkgs.indexOf(currentPackageName)));
+    }
+
+    private String getLockScreenCustomClockFace() {
+        String value = Settings.Secure.getStringForUser(mContext.getContentResolver(),
+                CUSTOM_CLOCK_FACE, USER_CURRENT);
+
+        if (value == null || value.isEmpty()) value = DEFAULT_CLOCK;
+
+        try {
+            JSONObject json = new JSONObject(value);
+            return json.getString("clock");
+        } catch (JSONException ex) {
+        }
+        return value;
+    }
+
+    private void setLockScreenCustomClockFace(String value) {
+        try {
+            JSONObject json = new JSONObject();
+            json.put("clock", value);
+            Settings.Secure.putStringForUser(mContext.getContentResolver(), CUSTOM_CLOCK_FACE,
+                    json.toString(), USER_CURRENT);
+        } catch (JSONException ex) {
+        }
     }
 }

387d639e2654efac655ea7158b62ff3331356940
SagarMakhar <sagarmakhar@gmail.com>
2021-07-07 09:04:07 +0000

    Add previews for statusbar icon packs
    Change-Id: I9f0888663e847c874ba834809168a14a69d6bd66
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- src/org/aospextended/extensions/fragments/Customisation.java
+++ src/org/aospextended/extensions/fragments/Customisation.java
@@ -82,11 +82,9 @@ public class Customisation extends SettingsPreferenceFragment implements OnPrefe
     private static final String TAG = "Customisation";
 
     private static final String SYSTEM_THEME_STYLE = "android.theme.customization.theme_style";
-    private static final String SYSTEM_ICON_STYLE = "android.theme.customization.icon_pack.android";
     private static final String SYSTEM_FONT_STYLE = "android.theme.customization.font";
 
     private ListPreference mSystemThemeStyle;
-    private ListPreference mIconPreference;
 
     private FontListPreference mFontPreference;
 
@@ -108,10 +106,6 @@ public class Customisation extends SettingsPreferenceFragment implements OnPrefe
         mSystemThemeStyle = (ListPreference) screen.findPreference(SYSTEM_THEME_STYLE);
         mSystemThemeStyle.setOnPreferenceChangeListener(this);
         updateState(mSystemThemeStyle);
-
-        mIconPreference = (ListPreference) screen.findPreference(SYSTEM_ICON_STYLE);
-        mIconPreference.setOnPreferenceChangeListener(this);
-        updateState(mIconPreference);
 
         mFontPreference = (FontListPreference) screen.findPreference(SYSTEM_FONT_STYLE);
         mFontPreference.setOnPreferenceChangeListener(this);
@@ -142,7 +136,6 @@ public class Customisation extends SettingsPreferenceFragment implements OnPrefe
     public void onResume() {
         super.onResume();
         updateState(mSystemThemeStyle);
-        updateState(mIconPreference);
         updateState((ListPreference) mFontPreference);
     }
 
@@ -151,10 +144,6 @@ public class Customisation extends SettingsPreferenceFragment implements OnPrefe
         if (preference == mSystemThemeStyle) {
             String value = (String) newValue;
             mThemeUtils.setThemeEnabled(value);
-            return true;
-        }
-        if (preference == mIconPreference) {
-            mThemeUtils.setOverlayEnabled(SYSTEM_ICON_STYLE, (String) newValue);
             return true;
         }
         if (preference == mFontPreference) {

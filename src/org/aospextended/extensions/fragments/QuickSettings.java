3a685f85f819cfd160d5b2b9b7b74b86bf480eca
spkal01 <kalligeross@gmail.com>
2021-07-11 16:15:23 +0530

    Extensions: Introduce A12 Brightness Slider [2/2]
    Change-Id: I30bd485fb31cb33e78e2a378d8b6ca89d8b432b3

--- src/org/aospextended/extensions/fragments/QuickSettings.java
+++ src/org/aospextended/extensions/fragments/QuickSettings.java
@@ -235,6 +235,8 @@ public class QuickSettings extends SettingsPreferenceFragment implements OnPrefe
                 Settings.System.SLIDER_STYLE , 0, UserHandle.USER_CURRENT) == 2;
         boolean sliderRUI = Settings.System.getIntForUser(getContext().getContentResolver(),
                 Settings.System.SLIDER_STYLE , 0, UserHandle.USER_CURRENT) == 3;
+        boolean sliderA12 = Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.SLIDER_STYLE , 0, UserHandle.USER_CURRENT) == 4;
 
         if (sliderDefault) {
             setDefaultSlider(mOverlayService);
@@ -244,6 +246,8 @@ public class QuickSettings extends SettingsPreferenceFragment implements OnPrefe
             enableSlider(mOverlayService, "com.android.theme.systemui_slider.aosp");
         } else if (sliderRUI) {
             enableSlider(mOverlayService, "com.android.theme.systemui_slider.rui");
+        } else if (sliderA12) {
+            enableSlider(mOverlayService, "com.android.theme.systemui_slider.a12");
         }
     }
 
@@ -277,7 +281,8 @@ public class QuickSettings extends SettingsPreferenceFragment implements OnPrefe
     public static final String[] SLIDERS = {
         "com.android.theme.systemui_slider_oos",
         "com.android.theme.systemui_slider.aosp",
-        "com.android.theme.systemui_slider.rui"
+        "com.android.theme.systemui_slider.rui",
+        "com.android.theme.systemui_slider.a12"
     };
 
     @Override

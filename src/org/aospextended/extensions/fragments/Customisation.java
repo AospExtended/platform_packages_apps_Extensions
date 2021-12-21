1f1cb332efe99625fd7555b41e8cf969b4489723
SagarMakhar <sagarmakhar@gmail.com>
2021-12-21 16:24:09 +0000

    [SQUASH] Bring back Udfps icons and Animations [2/3]
    Change-Id: I21db60139dceaa595324a2a35f05ed39006eec66
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- src/org/aospextended/extensions/fragments/Customisation.java
+++ src/org/aospextended/extensions/fragments/Customisation.java
@@ -51,6 +51,7 @@ import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceScreen;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.internal.util.aospextended.AEXUtils;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -88,6 +89,13 @@ public class Customisation extends SettingsPreferenceFragment implements OnPrefe
 
         final ContentResolver resolver = getActivity().getContentResolver();
         final PreferenceScreen screen = getPreferenceScreen();
+
+        boolean udfpsResPkgInstalled = AEXUtils.isPackageInstalled(getContext(),
+                "org.aospextended.udfps.resources");
+        PreferenceCategory udfps = (PreferenceCategory) screen.findPreference("udfps_category");
+        if (!udfpsResPkgInstalled) {
+            screen.removePreference(udfps);
+        }
     }
 
     @Override
